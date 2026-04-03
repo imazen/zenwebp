0105e8ccafb1b03d8049ecb2331082d7813310b6
Lilith River <lilith@imazen.io>
2026-04-03 02:26:01 -0600

    feat: add wasm128 tier to encoder TrueMotion prediction dispatch
    Add Wasm128Token variants for pred_luma16_tm and pred_chroma8_tm,
    falling back to scalar implementation. Adds wasm128 to incant!
    tier lists for complete platform coverage.

--- src/encoder/analysis/prediction.rs
+++ src/encoder/analysis/prediction.rs
@@ -107,7 +107,7 @@ pub fn pred_luma16_dc(dst: &mut [u8], left: Option<&[u8]>, top: Option<&[u8]>) {
 pub fn pred_luma16_tm(dst: &mut [u8], left_with_corner: Option<&[u8]>, top: Option<&[u8]>) {
     match (left_with_corner, top) {
         (Some(left), Some(top)) => {
-            incant!(pred_luma16_tm_impl(dst, left, top), [v3, neon, scalar]);
+            incant!(pred_luma16_tm_impl(dst, left, top), [v3, neon, wasm128, scalar]);
         }
         (Some(left), None) => {
             horizontal_pred(dst, Some(&left[1..17]), 16);
@@ -132,6 +132,12 @@ fn pred_luma16_tm_impl_v3(token: X64V3Token, dst: &mut [u8], left: &[u8], top: &
 #[inline(always)]
 fn pred_luma16_tm_impl_neon(token: NeonToken, dst: &mut [u8], left: &[u8], top: &[u8]) {
     pred_luma16_tm_neon(token, dst, left, top);
+}
+
+#[cfg(target_arch = "wasm32")]
+#[inline(always)]
+fn pred_luma16_tm_impl_wasm128(_token: Wasm128Token, dst: &mut [u8], left: &[u8], top: &[u8]) {
+    pred_luma16_tm_scalar(dst, left, top);
 }
 
 #[inline(always)]
@@ -263,7 +269,7 @@ pub fn pred_chroma8_dc(dst: &mut [u8], left: Option<&[u8]>, top: Option<&[u8]>) 
 pub fn pred_chroma8_tm(dst: &mut [u8], left_with_corner: Option<&[u8]>, top: Option<&[u8]>) {
     match (left_with_corner, top) {
         (Some(left), Some(top)) => {
-            incant!(pred_chroma8_tm_impl(dst, left, top), [v3, neon, scalar]);
+            incant!(pred_chroma8_tm_impl(dst, left, top), [v3, neon, wasm128, scalar]);
         }
         (Some(left), None) => {
             horizontal_pred(dst, Some(&left[1..9]), 8);
@@ -301,6 +307,12 @@ fn pred_chroma8_tm_impl_v3(token: X64V3Token, dst: &mut [u8], left: &[u8], top: 
 #[inline(always)]
 fn pred_chroma8_tm_impl_neon(token: NeonToken, dst: &mut [u8], left: &[u8], top: &[u8]) {
     pred_chroma8_tm_neon(token, dst, left, top);
+}
+
+#[cfg(target_arch = "wasm32")]
+#[inline(always)]
+fn pred_chroma8_tm_impl_wasm128(_token: Wasm128Token, dst: &mut [u8], left: &[u8], top: &[u8]) {
+    pred_chroma8_tm_scalar(dst, left, top);
 }
 
 #[inline(always)]
